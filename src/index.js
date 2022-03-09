e70c1020d41c2032e10f4aba58d1d87aa3751515
Khartanovich <kick00000@mail.ru>
2022-03-09 11:08:57 +0300

    init: add code for task Morse-decoder

--- src/index.js
+++ src/index.js
@@ -38,7 +38,15 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let lettersArr=[];
+    for(let start=0, end=10; end<=expr.length; start+=10, end+=10){
+        lettersArr=lettersArr.concat(expr.slice(start, end));
+    }
+    let withoutZeros=lettersArr.map(item => parseInt(item, 10));
+    let morseKey=withoutZeros.map(item => String(item).replace(/10/g, ".").replace(/11/g, "-"));
+    return morseKey.map(function(item, index){
+      return item == 'NaN'? item = ' ': MORSE_TABLE[morseKey[index]];}).join('')
+    
 }
 
 module.exports = {
